931dc2cee8e53c12238c5a5a1f9ed80b3083225a
alexliou <alexliou0@gmail.com>
2022-05-09 15:20:37 -0400

    Initial Commit

---
+++ HwSChallengeData/HwSChallengeDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  HwSChallengeDataApp.swift
+//  HwSChallengeData
+//
+//  Created by Alex Liou on 5/9/22.
+//
+
+import SwiftUI
+
+@main
+struct HwSChallengeDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
